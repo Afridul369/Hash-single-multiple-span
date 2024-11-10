6afbd7675cab6c9da1bb7ba7a83da70d8518b1f9
Afridul369 <afridul.dev@gmail.com>
2024-11-10 13:35:11 +0600

    details added

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -19,11 +19,13 @@
 import React from 'react'
 
 let Heading = ({text}) => {
-
+// =======learn this code====
 let abir = /#([^#]+)#/g;
 
+// =========here no need 2nd replace & need $1========
 let multiplespan = text.replace(abir,"<span class='hashcolor'>$1</span>");
 
+// =============here h1 has single end tag=======
   return (<h1 dangerouslySetInnerHTML={{__html:multiplespan}}/> 
   
 )
